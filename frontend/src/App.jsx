40a4c1dc86f8bdfd190b8b58985129e89a7ef306
Hritik-Thapa <hritikthapa331@gmail.com>
2024-11-26 13:05:29 +0545

    coment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,10 +6,13 @@ import { Button, Dropdown, Modal } from 'react-bootstrap'
 const App = () => {
   const [selected, setSelected] = useState('');
   const [modal, setModal] = useState(false);
+  const [data, setData] = useState({});
+
   return (
     <div className='flex flex-col items-center justify-center m-5 gap-4'>
       <p className='text-white bg-green-500 font-bold text-center text-4xl p-4 rounded-lg hover:text-green-500 hover:bg-white cursor-pointer w-[60%] '>Hello World</p>
       <Charts />
+
       <Dropdown>
         <Dropdown.Toggle variant='secondary' >
           {selected ? selected : 'Dropdown'}
@@ -20,24 +23,29 @@ const App = () => {
           <Dropdown.Item onClick={() => setSelected('Item 3')}>Item 3</Dropdown.Item>
         </Dropdown.Menu>
       </Dropdown>
+
+      {/* button for popup */}
+
       <Button variant='primary' className='p-4' onClick={() => setModal(true)}>Click Me</Button>
+
       {modal && (
         <Modal show={true} onHide={() => setModal(false)}>
-          <Modal.Dialog>
-            <Modal.Header closeButton>
-              <Modal.Title>Modal title</Modal.Title>
-            </Modal.Header>
+          <Modal.Header closeButton className='bg-slate-100'>
+            <Modal.Title >Modal title</Modal.Title>
+          </Modal.Header>
 
-            <Modal.Body>
-              <p>Modal body text goes here.</p>
-            </Modal.Body>
+          <Modal.Body className='w-[80%] m-3'>
+            <p>Modal body text goes here.</p>
+          </Modal.Body>
 
-            <Modal.Footer>
-              <Button variant="secondary" onClick={() => setModal(false)}>Close</Button>
-              <Button variant="primary">Save changes</Button>
-            </Modal.Footer>
-          </Modal.Dialog>
-        </Modal>)}
+          <Modal.Footer className='bg-slate-100' >
+            <Button variant="secondary" onClick={() => setModal(false)}>Close</Button>
+            <Button variant="primary">Save changes</Button>
+          </Modal.Footer>
+        </Modal>
+      )}
+
+
     </div>
   )
 }
