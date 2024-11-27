7916e6121d15bf77358e8bf7c3566d37d82d8e56
Raj-Timalsina <rajtimalsina2062@gmail.com>
2024-11-27 09:34:29 +0545

    Minute changes

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,27 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      backgroundImage: {
+        "custom-pattern": "url('./src/images/pattern-01.png')", // Single image
+        "gradient-radial": "radial-gradient(circle, #1E40AF, #1E3A8A)", // Gradient example
+        "hero-pattern": "url('https://example.com/hero-pattern.svg')",
+      },
       colors: {
-        brand: {
-          50: 'e3f2fd',
-          100: 'bbdefb',
-          200: '90caf9',
-          300: '64b5f6',
-          400: '42a5f5',
-          500: '2196f3',
-          600: '1e88e5',
-          700: '1976d2',
-          800: '1565c0',
-          900: '0d47a1',
-        }
-      }
+        neelo: {
+          50: "#e3f2fd",
+          100: "#bbdefb",
+          200: "#90caf9",
+          300: "#64b5f6",
+          400: "#42a5f5",
+          500: "#2196f3",
+          600: "#1e88e5",
+          700: "#1976d2",
+          800: "#1565c0",
+          900: "#0d47a1",
+        },
+      },
     },
     extend: {},
   },
   plugins: [],
-}
+};
